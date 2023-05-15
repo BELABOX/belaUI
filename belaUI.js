BELABOX/belaUI

commit d20433501bb6b205087588c9cccfbde1fc914a24

Faster Wifi connection status update on disconnection
We found that once a Wifi connection fails e.g. due to going out
of range or because the hotspot goes down, then the RUNNING flag
is immediately removed from the interface, while nmcli still shows
an established connection and the interface still has an IP
address configured for around 10 seconds longer

This patch uses a missing RUNNING flag to detect disconnections
earlier. Additionally, we trigger wifiScheduleScanUpdates() on
disconnection so that the network list can be automatically updated
to remove the AP that's now no longer in range. This seems to take
around 10-15 seconds, hence the additional polling added to
wifiScheduleScanUpdates()

belaUI.js
@@ -330,20 +330,21 @@ function updateNetif() {
         let inetAddr = int.match(/inet (\d+\.\d+\.\d+\.\d+)/);
         if (inetAddr) inetAddr = inetAddr[1];
 
+        const flags = int.match(/flags=\d+<([A-Z,]+)>/)[1].split(',');
+        const isRunning = flags.includes('RUNNING');
+
         // update the list of WiFi devices
         if (name && name.match('^wlan')) {
           let hwAddr = int.match(/ether ([0-9a-f:]+)/);
           if (hwAddr) {
-            wiFiDeviceListAdd(name, hwAddr[1], inetAddr);
+            wiFiDeviceListAdd(name, hwAddr[1], isRunning ? inetAddr : null);
           }
         }
 
         if (name == 'lo' || name.match('^docker') || name.match('^l4tbr')) continue;
 
         if (!inetAddr) continue;
-
-        const flags = int.match(/flags=\d+<([A-Z,]+)>/)[1].split(',');
-        if (!flags.includes('RUNNING')) continue;
+        if (!isRunning) continue;
 
         let txBytes = int.match(/TX packets \d+  bytes \d+/);
         txBytes = parseInt(txBytes[0].split(' ').pop());
@@ -1147,6 +1148,8 @@ function wifiScheduleScanUpdates() {
   setTimeout(wifiUpdateScanResult, 3000);
   setTimeout(wifiUpdateScanResult, 5000);
   setTimeout(wifiUpdateScanResult, 10000);
+  setTimeout(wifiUpdateScanResult, 15000);
+  setTimeout(wifiUpdateScanResult, 20000);
 }
 
 let unavailableDeviceRetryExpiry = 0;
@@ -1172,7 +1175,7 @@ function wifiUpdateDevices() {
   for (const networkDevice of networkDevices) {
     try {
       const [ifname, type, state, connUuid] = nmcliParseSep(networkDevice);
-      const conn = (connUuid != '') ? connUuid : null;
+      const conn = (connUuid != '' && wifiDeviceHwAddr[ifname].inetAddr) ? connUuid : null;
 
       if (type !== "wifi") continue;
       if (state == "unavailable") {
@@ -1228,6 +1231,7 @@ function wifiUpdateDevices() {
   }
   if (statusChange) {
     wifiUpdateScanResult();
+    wifiScheduleScanUpdates();
   }
   if (newDevices || statusChange) {
     wifiBroadcastState();

commit e492036c7c5d1ab8402c0c3b0e76608ee855f190

Tweak to avoid a harmless exception for non-wifi networks introduced by d20433501

belaUI.js
@@ -873,9 +873,15 @@ function wiFiDeviceListEndUpdate() {
   return wiFiDeviceListIsModified;
 }
 
-function wifiDeviceListGetAddr(ifname) {
+function wifiDeviceListGetHwAddr(ifname) {
   if (wifiDeviceHwAddr[ifname]) {
     return wifiDeviceHwAddr[ifname].hwAddr;
+  }
+}
+
+function wifiDeviceListGetInetAddr(ifname) {
+  if (wifiDeviceHwAddr[ifname]) {
+    return wifiDeviceHwAddr[ifname].inetAddr;
   }
 }
 
@@ -1122,7 +1128,7 @@ function wifiUpdateScanResult() {
 
     if (ssid == null || ssid == "") continue;
 
-    const hwAddr = wifiDeviceListGetAddr(device);
+    const hwAddr = wifiDeviceListGetHwAddr(device);
     if (!wifiIfs[hwAddr] || (active != 'yes' && wifiIfs[hwAddr].available.has(ssid))) continue;
 
     wifiIfs[hwAddr].available.set(ssid, {
@@ -1175,7 +1181,6 @@ function wifiUpdateDevices() {
   for (const networkDevice of networkDevices) {
     try {
       const [ifname, type, state, connUuid] = nmcliParseSep(networkDevice);
-      const conn = (connUuid != '' && wifiDeviceHwAddr[ifname].inetAddr) ? connUuid : null;
 
       if (type !== "wifi") continue;
       if (state == "unavailable") {
@@ -1183,7 +1188,8 @@ function wifiUpdateDevices() {
         continue;
       }
 
-      const hwAddr = wifiDeviceListGetAddr(ifname);
+      const conn = (connUuid != '' && wifiDeviceListGetInetAddr(ifname)) ? connUuid : null;
+      const hwAddr = wifiDeviceListGetHwAddr(ifname);
       if (!hwAddr) continue;
 
       if (wifiIfs[hwAddr]) {